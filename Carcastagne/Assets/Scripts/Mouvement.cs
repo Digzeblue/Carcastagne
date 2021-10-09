cec616ae4e15b28370cecf1dc584b4b55a9b1281
Antonin <antoninfouquet.pro@gmail.com>
2021-10-09 17:07:17 +0200

    les FREPABS

--- Carcastagne/Assets/Scripts/Mouvement.cs
+++ Carcastagne/Assets/Scripts/Mouvement.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Mouvement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float hauteurSaut = 5;
+    public float vitesse = 10;
+    PlayerControls control;
+    Rigidbody2D rb;
+    Vector2 move;
+
+    private void Awake()
     {
-        
+        control = new PlayerControls();
+        rb = GetComponent<Rigidbody2D>();
+
+        control.Gameplay.Jump.performed += ctx => Jump();
+        control.Gameplay.Move.performed += ctx => move = ctx.ReadValue<Vector2>();
+        control.Gameplay.Move.canceled += ctx => move = Vector2.zero;
     }
 
-    // Update is called once per frame
-    void Update()
+    void Jump()
     {
-        
+        rb.velocity = new Vector2(0,1) * hauteurSaut;
+    }
+
+    private void Update()
+    {
+        Vector2 m = new Vector2(move.x, 0)*vitesse*Time.deltaTime;
+        transform.Translate(m, Space.World);
+    }
+
+    private void OnEnable()
+    {
+        control.Gameplay.Enable();
+    }
+
+    private void OnDisable()
+    {
+        control.Gameplay.Disable();
     }
 }
